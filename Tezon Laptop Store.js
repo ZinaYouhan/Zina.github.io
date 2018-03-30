e1d19c82b417865749016c1121f639066f12c5a8
Zina-Youhan <zeenayouhan22@gmail.com>
2018-03-30 20:02:48 +0530

    update_task06

---
+++ Tezon Laptop Store.js
@@ -0,0 +1,386 @@
+$(document).ready(function()
+		{
+			$("#details311").click(function()
+			{
+				$("#details1").show();
+
+			});
+		});
+		$(document).ready(function()
+		{
+			$("#details311").mouseout(function()
+			{
+				$("#details1").hide();
+
+			});
+		});
+$(document).ready(function()
+		{
+			$("#details312").click(function()
+			{
+				$("#details2").show();
+
+			});
+		});
+		$(document).ready(function()
+		{
+			$("#details312").mouseout(function()
+			{
+				$("#details2").hide();
+
+			});
+		});	
+$(document).ready(function()
+		{
+			$("#details313").click(function()
+			{
+				$("#details3").show();
+
+			});
+		});
+		$(document).ready(function()
+		{
+			$("#details313").mouseout(function()
+			{
+				$("#details3").hide();
+
+			});
+		});
+$(document).ready(function()
+		{
+			$("#details314").click(function()
+			{
+				$("#details4").show();
+
+			});
+		});
+		$(document).ready(function()
+		{
+			$("#details314").mouseout(function()
+			{
+				$("#details4").hide();
+
+			});
+		});	
+$(document).ready(function()
+		{
+			$("#details317").click(function()
+			{
+				$("#details7").show();
+
+			});
+		});
+		$(document).ready(function()
+		{
+			$("#details317").mouseout(function()
+			{
+				$("#details7").hide();
+
+			});
+		});
+$(document).ready(function()
+		{
+			$("#details315").click(function()
+			{
+				$("#details5").show();
+
+			});
+		});
+		$(document).ready(function()
+		{
+			$("#details315").mouseout(function()
+			{
+				$("#details5").hide();
+
+			});
+		});
+$(document).ready(function()
+		{
+			$("#details318").click(function()
+			{
+				$("#details8").show();
+
+			});
+		});
+		$(document).ready(function()
+		{
+			$("#details318").mouseout(function()
+			{
+				$("#details8").hide();
+
+			});
+		});	
+$(document).ready(function()
+		{
+			$("#details319").click(function()
+			{
+				$("#details9").show();
+
+			});
+		});
+		$(document).ready(function()
+		{
+			$("#details319").mouseout(function()
+			{
+				$("#details9").hide();
+
+			});
+		});	
+$(document).ready(function()
+		{
+			$("#details110").click(function()
+			{
+				$("#details10").show();
+
+			});
+		});
+		$(document).ready(function()
+		{
+			$("#details110").mouseout(function()
+			{
+				$("#details10").hide();
+
+			});
+		});	
+$(document).ready(function()
+		{
+			$("#details111").click(function()
+			{
+				$("#details11").show();
+
+			});
+		});
+		$(document).ready(function()
+		{
+			$("#details111").mouseout(function()
+			{
+				$("#details11").hide();
+
+			});
+		});	
+$(document).ready(function()
+		{
+			$("#details112").click(function()
+			{
+				$("#details12").show();
+
+			});
+		});
+		$(document).ready(function()
+		{
+			$("#details112").mouseout(function()
+			{
+				$("#details12").hide();
+
+			});
+		});	
+$(document).ready(function()
+		{
+			$("#details113").click(function()
+			{
+				$("#details13").show();
+
+			});
+		});
+		$(document).ready(function()
+		{
+			$("#details113").mouseout(function()
+			{
+				$("#details13").hide();
+
+			});
+		});	
+$(document).ready(function()
+		{
+			$("#details114").click(function()
+			{
+				$("#details14").show();
+
+			});
+		});
+		$(document).ready(function()
+		{
+			$("#details114").mouseout(function()
+			{
+				$("#details14").hide();
+
+			});
+		});	
+$(document).ready(function()
+		{
+			$("#details115").click(function()
+			{
+				$("#details15").show();
+
+			});
+		});
+		$(document).ready(function()
+		{
+			$("#details115").mouseout(function()
+			{
+				$("#details15").hide();
+
+			});
+		});	
+$(document).ready(function()
+		{
+			$("#details116").click(function()
+			{
+				$("#details16").show();
+
+			});
+		});
+		$(document).ready(function()
+		{
+			$("#details116").mouseout(function()
+			{
+				$("#details16").hide();
+
+			});
+		});	
+$(document).ready(function()
+		{
+			$("#details117").click(function()
+			{
+				$("#details17").show();
+
+			});
+		});
+		$(document).ready(function()
+		{
+			$("#details117").mouseout(function()
+			{
+				$("#details17").hide();
+
+			});
+		});	
+$(document).ready(function()
+		{
+			$("#details118").click(function()
+			{
+				$("#details18").show();
+
+			});
+		});
+		$(document).ready(function()
+		{
+			$("#details118").mouseout(function()
+			{
+				$("#details18").hide();
+
+			});
+		});	
+$(document).ready(function()
+		{
+			$("#details119").click(function()
+			{
+				$("#details19").show();
+
+			});
+		});
+		$(document).ready(function()
+		{
+			$("#details119").mouseout(function()
+			{
+				$("#details19").hide();
+
+			});
+		});	
+$(document).ready(function()
+		{
+			$("#details120").click(function()
+			{
+				$("#details20").show();
+
+			});
+		});
+		$(document).ready(function()
+		{
+			$("#details120").mouseout(function()
+			{
+				$("#details20").hide();
+
+			});
+		});	
+$(document).ready(function()
+		{
+			$("#details121").click(function()
+			{
+				$("#details21").show();
+
+			});
+		});
+		$(document).ready(function()
+		{
+			$("#details121").mouseout(function()
+			{
+				$("#details21").hide();
+
+			});
+		});	
+$(document).ready(function()
+		{
+			$("#details122").click(function()
+			{
+				$("#details22").show();
+
+			});
+		});
+		$(document).ready(function()
+		{
+			$("#details122").mouseout(function()
+			{
+				$("#details22").hide();
+
+			});
+		});	
+$(document).ready(function()
+		{
+			$("#details123").click(function()
+			{
+				$("#details23").show();
+
+			});
+		});
+		$(document).ready(function()
+		{
+			$("#details123").mouseout(function()
+			{
+				$("#details23").hide();
+
+			});
+		});	
+$(document).ready(function()
+		{
+			$("#details124").click(function()
+			{
+				$("#details24").show();
+
+			});
+		});
+		$(document).ready(function()
+		{
+			$("#details124").mouseout(function()
+			{
+				$("#details24").hide();
+
+			});
+		});	
+$(document).ready(function()
+		{
+			$("#details125").click(function()
+			{
+				$("#details25").show();
+
+			});
+		});
+		$(document).ready(function()
+		{
+			$("#details125").mouseout(function()
+			{
+				$("#details25").hide();
+
+			});
+		});																																					
+
+						
